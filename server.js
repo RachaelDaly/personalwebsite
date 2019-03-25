ccb88dece61c6db2968debb1f9747dcdbb277b2f
RachaelDaly <rld5989@rit.edu>
2019-03-24 21:56:34 -0400

    Coloring and heading

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ s = require('express'),
 
 const PORT = process.env.PORT || 8080;
 
-fs.readFile('./src/index.html', function(err, html){
+fs.readFile('./src/html/index.html', function(err, html){
 	if(err)console.log(err);
 	http.createServer(function(req, res){
 		res.writeHeader(200, {"Content-Type": "text/html"});
