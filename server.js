de81fec398c3c08d7b049283692afbbe106b7be2
RachaelDaly <rld5989@rit.edu>
2019-03-25 14:05:18 -0400

    Got images to appear on server

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
-s = require('express'),
+var express = require('express'),
 	http = require('http'),
-	fs = require('fs');
+	fs = require('fs'),
+	path = require('path');
+
+var app = express();
+
+var publicDir = path.join(__dirname, '/public');
+app.use(express.static(publicDir));
 
 const PORT = process.env.PORT || 8080;
 
-fs.readFile('./src/html/index.html', function(err, html){
-	if(err)console.log(err);
-	http.createServer(function(req, res){
-		res.writeHeader(200, {"Content-Type": "text/html"});
-		res.write(html);
-		res.end();
-	}).listen(PORT);
+app.get('/', function(request, response){
+	fs.readFile('./public/html/index.html', function(err, html){
+		if(err)console.log(err);
+		response.writeHeader(200, {"Content-Type": "text/html"});
+		response.write(html);
+		response.end();
+	});
+});
+
+
+app.listen(PORT, function(){
+	console.log("Server is running on port 8080");
 });
