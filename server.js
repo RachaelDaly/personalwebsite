712618cd2667d4ed91fe64f86e68f4f36344df97
RachaelDaly <rld5989@rit.edu>
2019-03-26 11:16:12 -0400

    Added about page

--- server.js
+++ server.js
@@ -10,16 +10,25 @@ app.use(express.static(publicDir));
 
 const PORT = process.env.PORT || 8080;
 
-app.get('/', function(request, response){
+app.get('/', function(req, res){
 	fs.readFile('./public/html/index.html', function(err, html){
 		if(err)console.log(err);
-		response.writeHeader(200, {"Content-Type": "text/html"});
-		response.write(html);
-		response.end();
+		res.writeHeader(200, {'Content-Type': 'text/html'});
+		res.write(html);
+		res.end();
+	});
+});
+
+app.get('/about', function(req, res){
+	fs.readFile('./public/html/about.html', function(err, html){
+		if(err)console.log(err);
+		res.writeHeader(200, {'Content-Type': 'text/html'})
+		res.write(html);
+		res.end();
 	});
 });
 
 
 app.listen(PORT, function(){
-	console.log("Server is running on port 8080");
+	console.log('Server is running on port 8080');
 });
